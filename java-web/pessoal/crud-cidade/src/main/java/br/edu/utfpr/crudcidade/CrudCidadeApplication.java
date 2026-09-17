b9cda2d885975cb97cd92068adc026a840d5c1d2
AndreSanchesSouto <andresanchessouto@gmail.com>
2026-09-17 09:27:35 -0300

    feat: add java web start course

---
+++ java-web/pessoal/crud-cidade/src/main/java/br/edu/utfpr/crudcidade/CrudCidadeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.crudcidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCidadeApplication.class, args);
+	}
+
+}
