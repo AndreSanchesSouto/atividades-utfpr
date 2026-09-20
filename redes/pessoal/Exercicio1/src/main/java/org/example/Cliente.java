f1cbafc7ad0d17cbe6e48171ca03867a884413d2
andresanchessouto <andresanchessouto@gmail.com>
2026-09-20 12:41:30 -0300

    feat: add first activity of "redes"

---
+++ redes/pessoal/Exercicio1/src/main/java/org/example/Cliente.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Cliente {
+
+    private static Socket conexao;
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+
+    public static void main(String[] args) {
+        try {
+            conexao = new Socket("127.0.0.1", 55000);
+
+            String cpf = JOptionPane.showInputDialog( null, "Digite um CPF para verificação:" );
+
+            saida = new DataOutputStream(conexao.getOutputStream());
+            saida.writeUTF(cpf);
+
+            entrada = new DataInputStream(conexao.getInputStream());
+            String resposta = entrada.readUTF();
+            JOptionPane.showMessageDialog( null, "Resposta do servidor: " + resposta );
+
+        } catch (IOException exception) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, exception);
+        }
+    }
+}
---
+++ redes/pessoal/Exercicio1/src/main/java/org/example/Servidor.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Servidor {
+
+    private static ServerSocket servidor;
+    private static Socket conexao;
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+
+    public static void main(String args[]) {
+        try {
+            servidor = new ServerSocket(55000);
+            conexao = servidor.accept();
+
+            entrada = new DataInputStream(conexao.getInputStream());
+            String cpf = entrada.readUTF().trim().replaceAll("\\D", "");
+
+            String resultado = cpfEhValido(cpf) ? "Este CPF é válido" : "Este CPF é inválido";
+            saida = new DataOutputStream(conexao.getOutputStream());
+
+            saida.writeUTF(resultado);
+            conexao.close(); servidor.close();
+
+        } catch (IOException exception) {
+            Logger.getLogger(Servidor.class.getName()) .log(Level.SEVERE, null, exception);
+        }
+    }
+
+    private static boolean cpfEhValido(String cpf) {
+        if (cpf.length() != 11) return false;
+
+        char[] cpfChar = cpf.toCharArray();
+
+        int somador = 0;
+        for(int valor = 9; valor >= 1; valor--) {
+            somador += Character.getNumericValue(cpfChar[9 - valor]) * (valor + 1);
+        }
+
+        int resto = somador % 11;
+
+        int primeiroValidador = (resto < 2) ? 0 : (11 - resto);
+
+        somador = 0;
+
+        for (int valor = 10; valor >=1; valor--) {
+            int indice = 10 - valor;
+
+            somador += (indice == 9)
+                    ? primeiroValidador * (valor + 1)
+                    : Character.getNumericValue(cpfChar[indice]) * (valor + 1);
+
+        }
+
+        resto = somador % 11;
+
+        int segundoValidador = (resto < 2) ? 0 : (11 - resto);
+
+        return primeiroValidador == Character.getNumericValue(cpfChar[9])
+                && segundoValidador == Character.getNumericValue(cpfChar[10]);
+    }
+
+}
