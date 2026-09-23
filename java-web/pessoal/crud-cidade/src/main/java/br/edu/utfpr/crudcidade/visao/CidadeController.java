7916d49ac4221d08291b69e35ba091918c4a5f5f
AndreSanchesSouto <andresanchessouto@gmail.com>
2026-09-23 10:36:06 -0300

    feat: second week activity

--- java-web/pessoal/crud-cidade/src/main/java/br/edu/utfpr/crudcidade/visao/CidadeController.java
+++ java-web/pessoal/crud-cidade/src/main/java/br/edu/utfpr/crudcidade/visao/CidadeController.java
@@ -1,13 +1,25 @@
 package br.edu.utfpr.crudcidade.visao;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 
 @Controller
 public class CidadeController {
     @GetMapping("/")
-    public String listar() {
-        return "crud.html";
+    public String listar(Model memoria) {
+        final Set<Cidade> cidades = Set.of(
+                new Cidade("Cianorte", "PR"),
+                new Cidade("Maringá", "PR"),
+                new Cidade("Tapejara", "PR")
+
+        );
+
+        memoria.addAttribute("listaCidades", cidades);
+
+        return "/crud";
     }
 }
